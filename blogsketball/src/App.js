eb7f56458702e06a0bc5236c3d9b25abb6f266b8
Reese Kunz <reesekunz21@gmail.com>
2020-02-04 00:37:50 -0700

    initial set up

---
+++ blogsketball/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      This is my dope app
+    </div>
+  );
+}
+
+export default App;
---
+++ blogsketball/src/components/NavBar/NavBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NavBar() {
+  return <div className="nav-bar-container">This is the nav bar hellloooo</div>;
+}
+
+export default NavBar;
