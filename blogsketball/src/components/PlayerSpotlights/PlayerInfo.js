fd30a6c872964a9553397a4b5ffaf14be772e103
Reese Kunz <reesekunz21@gmail.com>
2020-03-27 22:17:16 -0700

    finished player spotlights

--- blogsketball/src/components/PlayerSpotlights/PlayerInfo.js
+++ blogsketball/src/components/PlayerSpotlights/PlayerInfo.js
@@ -3,49 +3,49 @@ import "./Player.scss";
 function PlayerInfo(props) {
   return (
     <div className="player-info-container">
-      <div className="section">
-        <p>
-          <strong>Position: </strong>
-          {props.player.position}
-        </p>
-        <p>
-          <strong>Height: </strong>
-          {props.player.height}
-        </p>
-        <p>
-          {" "}
-          <strong>Weight: </strong>
-          {props.player.weight}
-        </p>
-        <p>
-          <strong> College: </strong>
-          {props.player.college}
-        </p>
-        <p>
-          <strong>Pick: </strong>
-          {props.player.pick}
-        </p>
-        <p>
-          <strong>Player Comparison: </strong>
-          {props.player.player_comparison}
-        </p>
-      </div>
-      <div className="section">
-        <p>
-          <strong>2019-20: </strong> {props.player.year_stats}
-        </p>
-        <p>
-          <strong>Career: </strong>
-          {props.player.career_stats}
-        </p>
+      {/* <div className="section"> */}
+      <p>
+        <strong>Position: </strong>
+        {props.player.position}
+      </p>
+      <p>
+        <strong>Height: </strong>
+        {props.player.height}
+      </p>
+      <p>
+        {" "}
+        <strong>Weight: </strong>
+        {props.player.weight}
+      </p>
+      <p>
+        <strong>2019-20: </strong> {props.player.year_stats}
+      </p>
+      <p>
+        <strong>Career: </strong>
+        {props.player.career_stats}
+      </p>
 
-        <strong>Accolades: </strong>
-        <ul>
-          {props.player.accolades.map(each_accolade => (
-            <li key={each_accolade}>{each_accolade}</li>
-          ))}
-        </ul>
-      </div>
+      <p>
+        <strong>Player Comparison: </strong>
+        {props.player.player_comparison}
+      </p>
+      {/* </div>
+      <div className="section"> */}
+      <p>
+        <strong> College: </strong>
+        {props.player.college}
+      </p>
+      <p>
+        <strong>Pick: </strong>
+        {props.player.pick}
+      </p>
+      <strong>Accolades: </strong>
+      <ul>
+        {props.player.accolades.map(each_accolade => (
+          <li key={each_accolade}>{each_accolade}</li>
+        ))}
+      </ul>
+      {/* </div> */}
     </div>
   );
 }
